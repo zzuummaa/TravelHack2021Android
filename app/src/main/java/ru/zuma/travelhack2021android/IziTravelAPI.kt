c159c949bfabf48ed0acfb62f81eb1ee040964a0
Stepan <stepannk@mail.ru>
2021-03-27 14:42:15 +0300

    Init commit

---
+++ app/src/main/java/ru/zuma/travelhack2021android/IziTravelAPI.kt
@@ -0,0 +1,6 @@
+package ru.zuma.travelhack2021android
+
+import okhttp3.OkHttpClient
+
+private val client = OkHttpClient()
+
---
+++ app/src/main/java/ru/zuma/travelhack2021android/MainActivity.kt
@@ -0,0 +1,55 @@
+package ru.zuma.travelhack2021android
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.FragmentActivity
+import com.here.android.mpa.mapping.AndroidXMapFragment
+import com.here.android.mpa.common.OnEngineInitListener
+import com.here.android.mpa.mapping.Map
+import com.here.android.mpa.common.GeoCoordinate
+import java.io.File
+
+
+class MainActivity : FragmentActivity() {
+
+    // map embedded in the map fragment
+    private lateinit var map: Map
+
+    // map fragment embedded in this activity
+    private lateinit var mapFragment: AndroidXMapFragment
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initialize()
+    }
+
+    private fun initialize() {
+
+        // Search for the map fragment to finish setup by calling init().
+        mapFragment =
+            supportFragmentManager.findFragmentById(R.id.mapfragment) as AndroidXMapFragment
+
+        // Set up disk map cache path for this application
+        // Use path under your application folder for storing the disk cache
+        com.here.android.mpa.common.MapSettings.setDiskCacheRootPath(
+            getFilesDir().getPath() + File.separator + ".here-maps"
+        )
+
+        mapFragment.init(OnEngineInitListener { error ->
+            if (error == OnEngineInitListener.Error.NONE) {
+                // retrieve a reference of the map from the map fragment
+                map = mapFragment.getMap()!!
+                // Set the map center to the Vancouver region (no animation)
+                map.setCenter(
+                    GeoCoordinate(49.196261, -123.004773, 0.0),
+                    Map.Animation.NONE
+                )
+                // Set the zoom level to the average between min and max
+                map.setZoomLevel((map.getMaxZoomLevel() + map.getMinZoomLevel()) / 2)
+            } else {
+                Log.e(javaClass.simpleName, "Cannot initialize Map Fragment")
+            }
+        })
+    }
+}
